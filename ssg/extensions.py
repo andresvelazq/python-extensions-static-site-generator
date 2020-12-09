e63910191cf0a86e7ff7498cded42c16e9bdc7c5
Andres Velazquez <39769112+andresvelazq@users.noreply.github.com>
2020-12-09 15:40:02 -0500

    extensions

---
+++ ssg/extensions.py
@@ -0,0 +1,16 @@
+import importlib
+from pathlib import Path
+import sys
+
+def load_module(directory, name):
+    sys.path.insert(directory, 0)
+    importlib.import_module(name)
+    sys.path.pop(0)
+
+def load_directory(directory):
+    for path in directory:
+        load_module(directory.as_posix(), path.stem)
+
+def load_bundled():
+    directory = Path(__file__).parent / "extensions"
+    load_directory(directory)
